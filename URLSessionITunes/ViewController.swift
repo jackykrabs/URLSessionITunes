405e04d39e137babbe0707dd6e4882ada13a97ca
Jack Allen <17jba02@stu.cpsk12.org>
2017-05-24 11:25:49 -0500

    SDK? Not for me!
    You can now read and write to the firebase without the Firebase SDK,
    using the downloadData() method for reading ,and the addCollege()
    method for writing.

--- URLSessionITunes/ViewController.swift
+++ URLSessionITunes/ViewController.swift
@@ -26,15 +26,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         ref = Database.database().reference(withPath: "teams")
         table?.dataSource = self
         table?.delegate = self
-        //addCollege(abbrev: "MCSU", name: "Verles", region: "Mason Chander State")
-        // Do any additional setup after loading the view, typically from a nib.
-        downloadData() //downloads the top ten albums info using the traditional method
-        alamoFireExample() //downloads the top 50 country song info using the Alamofire Library
-        firebaseSDKExample() // downloads all 320 NCAA Colleges and sorts them into alphabetical order
-        
-        ref?.child("0").child("name").setValue("Blue Devils")
-
-        //table?.reloadData()
+        addCollege(abbrev: "LU", cid: 3, city: "St. Charles", did: 3, name: "Lions", pop: 66, region: "Lindenwood", state: "MO", rank: 3)
+        downloadData() //downloads the colleges like with firebaseSDK
     }
 
 
@@ -51,12 +44,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
-    //method to add college
-    func addCollege(abbrev: String, name: String, region: String){
-        ref?.child("320").child("name").setValue(name)
-        ref?.child("320").child("region").setValue(region)
-        ref?.child("320").child("abbrev").setValue(abbrev)
+    //method to add college (not using the firebase SDK because we're baller)
+    func addCollege(abbrev: String, cid: Int, city: String, did: Int, name: String, pop: Int, region: String, state: String, rank: Int){
+        
+        //create the json object to place into the database
+        let validTeam = [
+            "abbrev" : abbrev,
+            "cid" : cid,
+            "city" : city,
+            "did" : did,
+            "latitude" : 12.3,
+            "longitude" : 38.4,
+            "name" : name,
+            "pop" : pop,
+            "region" : region,
+            "state" : state,
+            "tid" : rank
+            ] as [String : Any]
+        
+        //put the 'team' we created and put it into the form of json data
+        let jsonData = try? JSONSerialization.data(withJSONObject: validTeam)
+        
+        //set up the URL request and set the request to 'POST' (let the program know we're writing data)
+        let requestURL: URL = URL(string: "https://my-awesome-project-8b957.firebaseio.com/teams.json")!
+        var urlRequest: URLRequest = URLRequest(url: requestURL as URL)
+        urlRequest.httpMethod = "POST"
+        
+        //move the data into the body of the http request (prepare to send off to the webserver aka Firebase project)
+        urlRequest.httpBody = jsonData
+        
+        //create the task to send the data and execute it
+        let task = URLSession.shared.dataTask(with: urlRequest as URLRequest) {
+            (data, response, error) -> Void in
+        }
+        task.resume()
     }
+ 
+    //sorts the array in alphabetical order (data isn't pulled in order since it's treated as a dictionary of json objects)
     func sortArray(){
         teams = teams.sorted { (s1: String, s2: String) -> Bool in
             return s1 < s2
@@ -83,11 +107,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     //method to download data using the URL libraries that apple already has
     func downloadData(){
-        //put url here and set up request settings and tsk
-        let requestURL: URL = URL(string: "https://itunes.apple.com/us/rss/topalbums/limit=10/json")!
-        let urlRequest: URLRequest = URLRequest(url: requestURL as URL)
-        let session = URLSession.shared
-        let task = session.dataTask(with: urlRequest as URLRequest) {
+        
+        //put url here, set up request settings, and set the HTTP method to 'GET' (let the program know we're reading data)
+        let requestURL: URL = URL(string: "https://my-awesome-project-8b957.firebaseio.com/teams.json")!
+        var urlRequest: URLRequest = URLRequest(url: requestURL as URL)
+        urlRequest.httpMethod = "GET"
+        
+        //create the task to read the data, and then execute it (.resume())
+        let task = URLSession.shared.dataTask(with: urlRequest as URLRequest) {
             (data, response, error) -> Void in
             
             let httpResponse = response as! HTTPURLResponse
@@ -96,26 +123,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             //if it's all good in the hood, start parsing the json
             if(statusCode == 200){
                 do{
-                    var json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String : AnyObject]
-                    
-                    if let feed = json["feed"] as? [String: Any]{
-                        if let entry = feed["entry"] as? [Any] {
-                            for index in entry{
-                                if let currentEntry = index as? [String: Any] {
-                                    if let imName = currentEntry["im:name"] as? [String: Any]{
-                                        if let label = imName["label"] as? String {
-                                            self.albumTitles.append(label)
-                                        }
-                                    }
-                                    if let albumPrice = currentEntry["im:price"] as? [String: Any]{
-                                        if let label = albumPrice["label"] as? String {
-                                            self.albumPrices.append(label)
-                                        }
-                                    }
-                                }
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! Dictionary <String, AnyObject>
+                   
+                    //iterate through the dictionary of json objects, and add each one's school name and mascot to the (same) cell in the teams array
+                    for (_, value) in json {
+                        if let team = value as? [String: AnyObject]{
+                            if let name = team["region"] as? String {
+                                self.teams.append(name)
+                            }
+                            if let school = team["name"] as? String {
+                                self.teams[self.teams.count - 1] = self.teams[self.teams.count - 1] + " " + school
                             }
                         }
                     }
+                    //if this isn't called, the teams are in a random order
+                    self.sortArray()
+                    
+                    //update the table with the new data
                     self.table?.reloadData()
                     
                 }catch{
@@ -131,8 +155,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
         Alamofire.request("https://itunes.apple.com/us/rss/topsongs/limit=50/genre=6/json").responseJSON { (response)->Void in
             //go crazy with alamofire
-
-            
             if let json = response.result.value as? [String: AnyObject]{
                 if let feed = json["feed"] as? [String: Any]{
                     if let entry = feed["entry"] as? [Any]{
